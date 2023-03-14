91076238a423c14bc99d3983f6fda5d63a2240cf
DenisHShingala <denis.s@simformssolutions.com>
2023-03-14 19:04:56 +0530

    v1.0

---
+++ JS/index.js
@@ -0,0 +1,35 @@
+const targetDiv = document.getElementById('page-preview');
+const links = document.querySelectorAll('.navlink');
+
+const removeActiveClass = () => {
+    links.forEach((ele) => {
+        ele.classList.remove('active-link');
+    })
+}
+
+// create xml object
+const xml = new XMLHttpRequest();
+
+const fetchData = (event) => {
+    console.log('clicked')
+    xml.open('GET', "./pages/" + event.target.dataset.viewPage + ".html", true);
+    xml.send();
+    xml.onerror = (err) => {
+        console.log(err);
+    }
+
+    xml.onreadystatechange = () => {
+        if (xml.readyState === 4) {
+            targetDiv.innerHTML = xml.responseText;
+            removeActiveClass();
+            event.target.classList.add('active-link');
+        }
+    }
+}
+
+links.forEach((ele) => {
+    ele.addEventListener('click', fetchData);
+    if (ele.dataset.dafault) {
+        ele.initEvent
+    }
+});
